988d5beda1bf8f8d147f1cce70c547ce0c0728c9
Sunray777 <alpzm7@gmail.com>
2020-12-25 15:26:21 +0300

    new

---
+++ js/newtip.js
@@ -0,0 +1,29 @@
+// Однострочный комментарий
+
+/*
+Многострочный комментарий
+*/
+
+/**JSDoc комментарии
+ * 
+ */
+
+ /** Типы данных:
+  * 1. Примитивные
+  * - Number: 2019, 1.5, Nan, Infinity
+  * - String: 'Heelo', "Hello", `Hello`
+  * - Boolean: true, false
+  * - Null: nuLL
+  * - Undefined: undefined
+  * - SymboL()
+  */
+
+  /**
+   * 2. Объекты (Reference type) Передаются по ссылке.
+   * - Object: { name: 'Alexander', age: '33' }
+   * * Array(Массив) [1, 2, 3] 
+   * * Function: function foo() {}
+   * * Date: new Date()
+   */
+
+   
