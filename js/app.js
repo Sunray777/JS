8e17f45917f83a8801783b018e141d07acac94cc
Sunray777 <alpzm7@gmail.com>
2021-01-20 10:36:56 +0300

    Все задачи решил

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 function guessTheNumber(num) {
   if (num <= 10 && num >= 0) {
-    function random(num) {
-      let res = Math.ceil(Math.random(num));
-      console.log(res);
-      return res;
-    }
-    
+    res = Math.ceil(Math.random() * num) + 1;
+    console.log(res);
+    return res;
   } else if (num !== "number") {
     return new Error("Please provide a valid number");
   } else {
