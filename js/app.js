f894901e29c6daaf534f40a2ccd3d094f75312d9
Sunray777 <alpzm7@gmail.com>
2021-02-09 08:44:19 +0300

    Занимаюсь

--- js/app.js
+++ js/app.js
@@ -97,7 +97,17 @@ function handler1(el) {
   return el.length;
 }
 
-function nameToUpperCase(el) {
+function nameToUpperCase(el) { //Пытаюсь склеить слова, и чтобы каждое слово было с большой буквы
+let resNew = [];
+  for (i = 0; i < el.length; i++)
+  {
+    if (i === 0 || el[i - 1 === " "])
+{resNew += el[i].toUpperCase()
+}
+  }
+
+
+
   return el.toUpperCase();
 }
 
@@ -106,6 +116,7 @@ const result2 = firstFunc(["my", "name", "is", "Trinity"], nameToUpperCase);
 
 console.log(result);
 console.log(result2);
+
 
 // console.log(firstFunc(["my", "name", "is", "Trinity"], handler1));
 
